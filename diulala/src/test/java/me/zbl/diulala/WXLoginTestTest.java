f6a46f6e0c27177945a3604727fbc92cf052f6c7
JamesZBL <1146556298@qq.com>
2018-05-09 13:06:33 +0800

    改用 Feign 发送 http 请求，解决返回 invalid appid 的问题

--- diulala/src/test/java/me/zbl/diulala/WXLoginTestTest.java
+++ diulala/src/test/java/me/zbl/diulala/WXLoginTestTest.java
@@ -1,10 +1,13 @@
+package me.zbl.diulala;
+
 import feign.Feign;
 import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
+import me.zbl.diulala.client.WXApi;
+import me.zbl.diulala.entity.response.ApiLoginResponse;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Map;
 
 /**
  * @author JamesZBL
@@ -13,24 +16,28 @@ import java.util.Map;
  */
 public class WXLoginTestTest {
 
+  private WXApi wxApi;
+
+  @Before
+  public void setUp(){
+    wxApi = Feign.builder()
+            .decoder(new GsonDecoder())
+            .encoder(new GsonEncoder())
+            .target(WXApi.class, "https://api.weixin.qq.com");
+  }
 
   /**
    * 测试小程序登录
    */
   @Test
   public void testWXLogin() {
-    WXApi wxApi = Feign.builder()
-            .decoder(new GsonDecoder())
-            .encoder(new GsonEncoder())
-            .target(WXApi.class, "https://api.weixin.qq.com");
-
-    Map<String, String> response = wxApi.loginPlain(
+    ApiLoginResponse response = wxApi.login(
             "wx698220e7667a01e3",
             "882d28c93d94c8b7a5450fd0d45a052f",
             "123456",
             "authorization_code"
     );
+    Assert.assertNotNull(response.getOpenid());
     System.out.println(response);
-    Assert.assertNotNull(response);
   }
 }
